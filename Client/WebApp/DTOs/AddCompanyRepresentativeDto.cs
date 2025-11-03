a131bbe3ffd290dc7e64e6919edee443ac9919cc
baczek113 <baczek.jakub@gmail.com>
2025-11-03 16:15:27 +0100

    registering company representative works all the way through the application

---
+++ Client/WebApp/DTOs/AddCompanyRepresentativeDto.cs
@@ -0,0 +1,9 @@
+namespace DTOs;
+
+public class AddCompanyRepresentativeDto
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Position { get; set; }
+    public string Password { get; set; }
+}
