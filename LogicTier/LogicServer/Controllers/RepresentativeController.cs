701597282a53307cb52a658e955cfb079e0a06a7
Damian Choina <68649009+Damizen@users.noreply.github.com>
2025-11-26 16:59:53 +0100

    Fixed duplicated email bug in Logic Tier

--- LogicTier/LogicServer/Controllers/RepresentativeController.cs
+++ LogicTier/LogicServer/Controllers/RepresentativeController.cs
@@ -1,4 +1,5 @@
-﻿using LogicServer.DTOs.Representative;
+﻿using Grpc.Core;
+using LogicServer.DTOs.Representative;
 using LogicServer.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,21 +10,36 @@ namespace LogicServer.Controllers;
 public class RepresentativeController : ControllerBase
 {
     private readonly RepresentativeService _service;
-    
-    public RepresentativeController(RepresentativeService service)
+    private readonly ILogger<RepresentativeController> _logger;
+
+    public RepresentativeController(RepresentativeService service, ILogger<RepresentativeController> logger)
     {
         _service = service;
+        _logger = logger;
     }
 
     [HttpPost]
     public async Task<ActionResult<RepresentativeDto>> CreateRepresentative([FromBody] CreateRepresentativeDto dto)
     {
-        if (dto == null)
+        try
         {
-            return BadRequest("Invalid data");
-        }
+            if (dto == null)
+            {
+                return BadRequest("Invalid data");
+            }
 
-        var result = await _service.CreateRepresentativeAsync(dto);
-        return Ok(result);
+            var result = await _service.CreateRepresentativeAsync(dto);
+            return Ok(result);
+        }
+        catch (RpcException e) when (e.StatusCode == Grpc.Core.StatusCode.AlreadyExists)
+        {
+            _logger.LogWarning("Failed to create representative: {Detail} ", e.Status.Detail);
+            return Conflict(e.Status.Detail);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Internal Server Error while creating representative");
+            return StatusCode(500, e.Message);
+        }
     }
 }
