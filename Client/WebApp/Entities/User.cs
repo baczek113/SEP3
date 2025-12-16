a0f05cd49e41f62006367a7d32ebb32055505a78
baczek113 <baczek.jakub@gmail.com>
2025-12-16 21:07:24 +0100

    removed unnecessary folder

--- Client/WebApp/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApp.Entities;
-
-public class User
-{
-    public string Name { get; set; }
-    public int Id { get; set; }
-    public string Email { get; set; }
-    public string Role { get; set; }
-}
